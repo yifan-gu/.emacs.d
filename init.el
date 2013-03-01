da6f66118c0ab9a2d3aba0a55e027f4c485a0f7f
Ivan7702 <guyifan1121@gmail.com>
2013-03-01 16:54:01 +0800

    modified:   ac-comphist.dat 	modified:   init.el 	new file:   lisp/web-mode.el

--- init.el
+++ init.el
@@ -54,8 +54,8 @@
 (set-default-font "Monospace-10")
 ;(set-frame-font "Monaco-10")
 
-
 ; indent
+(setq js-indent-level 2)
 (setq indent-tabs-mode nil)
 (setq default-tab-width 4)
 (setq tab-width 4)
@@ -248,8 +248,33 @@
 
 (add-to-list 'ac-modes 'shell-mode)
 (add-hook 'shell-mode-hook 'ac-rlc-setup-sources)
-;-------------------------------------------------------------------------------------
+;------------------------------------------------------------------------------------
 
+;;------------web mode--------------
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;(set-face-attribute 'web-mode-doctype-face nil :foreground "green")
+;(set-face-attribute 'web-mode-html-tag-face nil :bold t :foreground "#edd440")
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "#edd440")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "tomato")
+;(set-face-attribute 'web-mode-html-attr-value-face nil :foreground "8ae234")
+
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook 'web-mode-hook)
+(add-to-list 'web-mode-snippets '("mydiv" "<div>" "</div>"))
+;----------end----------------------
 ;vi emulation
 ;(add-to-list 'load-path "~/.emacs.d/lisp/evil")
 ;(require 'evil)
