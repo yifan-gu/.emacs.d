7579de4edfa7503cc7668dce8bd523484683b228
evan-gu <guyifan1121@gmail.com>
2014-01-30 12:22:38 -0500

    add map

--- keyremapping/remapkey.sh
+++ keyremapping/remapkey.sh
@@ -9,7 +9,11 @@ xmodmap -e "keycode 22 = backslash"
 
 # remap Caps_Lock to Control_L
 # remap Control_L to Caps_Lock
-xmodmap -e "clear lock"             
+xmodmap -e "clear lock"
+xmodmap -e "keysym Control_L = Caps_Lock"
 xmodmap -e "keysym Caps_Lock = Control_L"
 xmodmap -e "add Lock = Caps_Lock"
+xmodmap -e "add Control = Control_L"
 xmodmap -e "keycode 37 = Caps_Lock"
+xmodmap -e "keycode 66 = Control_L"
+
