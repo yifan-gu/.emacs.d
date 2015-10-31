695bb3a28296d4f23d92ad1f8ebe96cc5d5b9d5c
Yifan Gu <yifan.gu@coreos.com>
2015-10-30 18:42:33 -0700

    yaml mode

--- init.el
+++ init.el
@@ -601,3 +601,12 @@
                     ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
                     ))
              ) t)
+
+;; yaml mode
+(add-to-list 'load-path "~/.emacs.d/lisp/yaml-mode/")
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-hook 'yaml-mode-hook
+      '(lambda ()
+        (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
